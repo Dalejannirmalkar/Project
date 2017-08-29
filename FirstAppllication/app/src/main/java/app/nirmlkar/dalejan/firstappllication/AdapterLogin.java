97020b449930d1c99a6ff6eff03445d24435d4bc
Dalejannirmalkar <dalejannirmalkar@gmail.com>
2017-08-30 01:51:34 +0530

    Update AdapterLogin.java

--- FirstAppllication/app/src/main/java/app/nirmlkar/dalejan/firstappllication/AdapterLogin.java
+++ FirstAppllication/app/src/main/java/app/nirmlkar/dalejan/firstappllication/AdapterLogin.java
@@ -1,6 +1,6 @@
 package app.nirmlkar.dalejan.firstappllication;
 
-import android.support.v7.widget.Recycler;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
